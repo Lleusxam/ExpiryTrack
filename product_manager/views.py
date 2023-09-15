be87321a675a963e5802648c3acba3aed835a294
Evandir Filho <evandirfilho@gmail.com>
2023-09-15 18:31:28 -0300

    Add: Adiçao do template e das paginas home e listagem

--- product_manager/views.py
+++ product_manager/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from product_manager.models import Product
 # Create your views here.
 
 def home(request):
-    return render(request, "index.html")
+    return render(request, "bootstrap-admin-template-free/home.html")
+
+def listagem(request):
+    products = Product.objects.all()
+    return render(request, 'bootstrap-admin-template-free/listagem.html', {'products': products})
